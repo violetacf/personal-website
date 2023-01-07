4a2e2ffd94df712ac337eb124d07f82571f4c3d5
violetacf <violeta@cique.es>
2023-01-07 14:56:22 +0000

    test6

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,19 +3,17 @@ import MyPortfolio from '../My-portfolio/my-portfolio';
 import Contact from '../Contact/contact';
 import About from '../About/about';
 import Navbar from '../Navbar/navbar';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<About />} />
           <Route path="portfolio" element={<MyPortfolio />} />
           {/* <Route path="/contact" element={<Contact />} /> */}
         </Routes>
-      </BrowserRouter>
     </div>
   );
 }
